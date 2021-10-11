7be68eb119ec6ed9de23741d2c841788456c789a
петр николаев <nikolaev_pe@mail.ru>
2021-10-12 03:32:37 +0500

    Добавил swagger

--- ClothesAPI/Startup.cs
+++ ClothesAPI/Startup.cs
@@ -1,29 +1,25 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
 
 namespace ClothesAPI
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo 
+                {
+                    Description = "Swagger API v1",
+                    Title = "Swagger for products",
+                    Version = "1.0.0"
+                });
+            });
             services.AddControllers();
         }
 
@@ -33,6 +29,16 @@ namespace ClothesAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger UI");
+                options.DocumentTitle = "Products";
+                options.RoutePrefix = "docs";
+                options.DocExpansion(DocExpansion.List);
+
+            });
 
             app.UseHttpsRedirection();
 
